f84401a29f84c3463af92ce38eb45d50b8d8deea
axeljavier-alvarez <axjavier4alvarez44@gmail.com>
2026-06-30 13:55:07 -0600

    cambios

---
+++ resources/views/admin/dashboard.blade.php
@@ -0,0 +1,3 @@
+<h2>
+    dashboard
+</h2>
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
-Route::get('/', function () {
-    return view('welcome');
-});
+use App\Http\Controllers\Admin\AdminController;
+Route::get('/', [AdminController::class, 'login'])->name('admin.login');
+Route::post('admin/auth', [AdminController::class, 'auth'])->name('admin.auth');
+Route::get('dashboard', [AdminController::class, 'index'])->name('admin.index');
